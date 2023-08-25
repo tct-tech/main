bb00776591713b4207da129594079cb9be60a465
tct-tech <lemuellina1@gmail.com>
2023-08-25 14:13:58 -0700

    setup deployment

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@
  * @Author: Your name
  * @Date:   2023-08-25 13:54:30
  * @Last Modified by:   Your name
- * @Last Modified time: 2023-08-25 13:57:05
+ * @Last Modified time: 2023-08-25 14:13:34
  */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "base/main",
+  base: "/main",
   plugins: [react()],
 })
